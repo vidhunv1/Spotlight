a5cdbcb20e3d7e8faea7b771bb39395130275a56
Vidhun Vinod <vidhunv1@gmail.com>
2016-07-04 22:02:21 +0530

    Initial commit, Launcher Activity

---
+++ app/src/main/java/com/stairway/spotlight/ui/flows/LauncherActivity.java
@@ -0,0 +1,15 @@
+package com.stairway.spotlight.ui.flows;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stairway.spotlight.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
