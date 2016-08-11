2a59ede08228a508bd2e4b60ae764a6003cd2af8
Vidhun Vinod <vidhunv1@gmail.com>
2016-08-12 03:34:47 +0530

    complete messages screen

---
+++ app/src/main/java/com/stairway/spotlight/screens/message/MessageActivity.java
@@ -0,0 +1,118 @@
+package com.stairway.spotlight.screens.message;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+
+import com.stairway.data.manager.Logger;
+import com.stairway.data.source.message.MessageResult;
+import com.stairway.spotlight.R;
+import com.stairway.spotlight.core.di.component.ComponentContainer;
+import com.stairway.spotlight.core.BaseActivity;
+import com.stairway.data.manager.XMPPManager;
+import com.stairway.spotlight.screens.message.di.MessageModule;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MessageActivity extends BaseActivity implements MessageContract.View {
+    @Inject
+    public XMPPManager connection;
+
+    @Inject
+    MessagePresenter messagePresenter;
+
+    @Bind(R.id.rv_messageitem)
+    RecyclerView messageItem;
+
+    @Bind(R.id.et_sendmessage_message)
+    EditText messageBox;
+
+    private static String USER_ID = "USERID";
+    private String chatId;
+    private String currentUser;
+    private MessagesAdapter messagesAdapter;
+
+
+    public static Intent callingIntent(Context context, long userId) {
+        Intent intent = new Intent(context, MessageActivity.class);
+        intent.putExtra(USER_ID, userId);
+
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        ButterKnife.bind(this);
+        messagesAdapter = new MessagesAdapter(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        messageItem.setLayoutManager(linearLayoutManager);
+
+        messageItem.setAdapter(messagesAdapter);
+
+
+        Intent receivedIntent = getIntent();
+        if(!receivedIntent.hasExtra(USER_ID))
+            return;
+
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+        chatId = Long.toString(receivedIntent.getLongExtra(USER_ID, 0));
+        ab.setTitle(chatId);
+
+        messagePresenter.attachView(this);
+        messagePresenter.loadMessages(chatId);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        messagePresenter.detachView();
+    }
+
+    @OnClick(R.id.btn_sendMessage_send)
+    public void onSendClicked() {
+        String message = messageBox.getText().toString();
+        messageBox.setText("");
+        messagePresenter.sendMessage(new MessageResult(chatId, currentUser, message, MessageResult.DeliveryStatus.NOT_SENT));
+    }
+
+    @Override
+    public void displayMessages(List<MessageResult> messages) {
+        messagesAdapter.setMessages(messages);
+        Logger.d("Init messages list");
+    }
+
+    @Override
+    public void addMessageToList(MessageResult message) {
+        messagesAdapter.addMessage(message);
+        messageItem.scrollToPosition(messagesAdapter.getItemCount()-1);
+    }
+
+    @Override
+    public void updateDeliveryStatus(MessageResult messageResult) {
+        messagesAdapter.updateMessage(messageResult);
+    }
+
+    @Override
+    protected void injectComponent(ComponentContainer componentContainer) {
+        componentContainer.userSessionComponent().plus(new MessageModule()).inject(this);
+        currentUser = componentContainer.userSessionComponent().getUserSession().getUserId();
+    }
+}
---
+++ app/src/main/java/com/stairway/spotlight/screens/message/MessagesAdapter.java
@@ -0,0 +1,101 @@
+package com.stairway.spotlight.screens.message;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.stairway.data.manager.Logger;
+import com.stairway.data.source.message.MessageResult;
+import com.stairway.spotlight.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by vidhun on 07/08/16.
+ */
+public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHolder>{
+    //TODO: Change to cursor recycler view adapter.
+
+    private Context context;
+    private List<MessageResult> messageList;
+
+    public MessagesAdapter(Context context) {
+        this.context = context;
+        this.messageList = new ArrayList<>();
+    }
+
+    public void setMessages(List<MessageResult> messages) {
+        this.messageList.clear();
+        this.messageList.addAll(messages);
+        this.notifyItemRangeInserted(0, messageList.size() - 1);
+    }
+
+    public void addMessage(MessageResult messageResult) {
+        messageList.add(messageResult);
+        this.notifyItemInserted(messageList.size()-1);
+    }
+
+    public void updateMessage(MessageResult messageResult) {
+        int position = 0;
+        for(MessageResult m: messageList) {
+            if(messageResult.getMessageId().equals(m.getMessageId())) {
+                messageList.set(position, messageResult);
+                this.notifyItemChanged(position);
+                break;
+            }
+            position++;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.message_list_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.renderItem(messageList.get(position));
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.tv_messageItem_contactName)
+        TextView contactName;
+
+        @Bind(R.id.tv_messageitem_message)
+        TextView message;
+
+        @Bind(R.id.tv_messageitem_time)
+        TextView time;
+
+        @Bind(R.id.tv_messageitem_deliverystatus)
+        TextView deliveryStatus;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void renderItem(MessageResult messageResult) {
+            contactName.setText(messageResult.getFromId());
+            message.setText(messageResult.getMessage());
+            time.setText("09:45");
+            deliveryStatus.setText(String.valueOf(messageResult.getDeliveryStatus().ordinal()));
+        }
+    }
+}
