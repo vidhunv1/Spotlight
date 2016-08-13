778be314436cffb32165893b20b440299203ff54
Vidhun Vinod <vidhunv1@gmail.com>
2016-08-13 22:41:06 +0530

    reset LoadMessagesUseCase

--- app/src/main/java/com/stairway/spotlight/screens/message/LoadMessagesUseCase.java
+++ app/src/main/java/com/stairway/spotlight/screens/message/LoadMessagesUseCase.java
@@ -1,19 +1,51 @@
-Unstaged changes after reset:
-M	app/src/main/java/com/stairway/spotlight/screens/home/chatlist/ChatListAdapter.java
-M	app/src/main/java/com/stairway/spotlight/screens/home/chatlist/ChatListFragment.java
-M	app/src/main/java/com/stairway/spotlight/screens/home/chatlist/ChatListItemModel.java
-M	app/src/main/java/com/stairway/spotlight/screens/home/chatlist/ChatListPresenter.java
-D	app/src/main/java/com/stairway/spotlight/screens/message/GetMessagesUseCase.java
-M	app/src/main/java/com/stairway/spotlight/screens/message/LoadMessagesUseCase.java
-M	app/src/main/java/com/stairway/spotlight/screens/message/MessageActivity.java
-M	app/src/main/java/com/stairway/spotlight/screens/message/MessageContract.java
-M	app/src/main/java/com/stairway/spotlight/screens/message/MessagePresenter.java
-M	app/src/main/java/com/stairway/spotlight/screens/message/MessagesAdapter.java
-M	app/src/main/java/com/stairway/spotlight/screens/message/ReceiveMessagesUseCase.java
-M	app/src/main/java/com/stairway/spotlight/screens/message/di/MessageModule.java
-M	data/build.gradle
-M	data/src/main/java/com/stairway/data/manager/XMPPManager.java
-M	data/src/main/java/com/stairway/data/source/auth/UserSessionResult.java
-M	data/src/main/java/com/stairway/data/source/message/MessageApi.java
-M	data/src/main/java/com/stairway/data/source/message/MessageResult.java
-M	data/src/main/java/com/stairway/data/source/message/MessageStore.java
+package com.stairway.spotlight.screens.message;
+
+import com.stairway.data.manager.Logger;
+import com.stairway.data.source.message.MessageApi;
+import com.stairway.data.source.message.MessageResult;
+import com.stairway.data.source.message.MessageStore;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * Created by vidhun on 06/08/16.
+ */
+public class LoadMessagesUseCase {
+    private MessageStore messageStore;
+    private MessageApi messageApi;
+
+    @Inject
+    public LoadMessagesUseCase(MessageApi messageApi, MessageStore messageStore) {
+        this.messageApi = messageApi;
+        this.messageStore = messageStore;
+    }
+
+    public Observable<MessageResult> execute(String chatId) {
+
+        Observable<MessageResult> getMessages = Observable.create(subscriber -> {
+            messageStore.getMessages(chatId).subscribe(new Subscriber<MessageResult>() {
+                @Override
+                public void onCompleted() {
+                    subscriber.onCompleted();
+                }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+
+                @Override
+                public void onNext(MessageResult messageResults) {
+                    subscriber.onNext(messageResults);
+                }
+            });
+        });
+
+        return getMessages;
+    }
+}
