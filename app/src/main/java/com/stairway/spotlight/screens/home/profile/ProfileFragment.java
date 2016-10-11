7dea4337929b3868b0a124b7d2bbb14991eacbbc
Vidhun <vidhunv1@gmail.com>
2016-10-11 22:10:00 +0530

    My profile fragment

--- app/src/main/java/com/stairway/spotlight/screens/home/profile/ProfileFragment.java
+++ app/src/main/java/com/stairway/spotlight/screens/home/profile/ProfileFragment.java
@@ -5,16 +5,23 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.stairway.spotlight.R;
 import com.stairway.spotlight.core.BaseFragment;
 import com.stairway.spotlight.core.di.component.ComponentContainer;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by vidhun on 04/10/16.
  */
 
 public class ProfileFragment extends BaseFragment {
+    @Bind(R.id.iv_profileImage)
+    ImageView profileImage;
+
     public ProfileFragment() {
     }
 
@@ -32,6 +39,8 @@ public class ProfileFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+        profileImage.setImageResource(R.drawable.default_profile_image);
         return view;
     }
 
