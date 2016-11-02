9a5458a3793c145f93f30dab209b364af4c074b7
Vidhun <vidhunv1@gmail.com>
2016-11-03 03:04:19 +0530

    initial working for navigation menu

--- app/src/main/java/com/stairway/spotlight/screens/home/HomeActivity.java
+++ app/src/main/java/com/stairway/spotlight/screens/home/HomeActivity.java
@@ -1,55 +1,35 @@
 package com.stairway.spotlight.screens.home;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.view.LayoutInflater;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.stairway.data.manager.Logger;
-import com.stairway.data.source.user.UserAuthApi;
-import com.stairway.data.source.user.UserSessionResult;
-import com.stairway.data.source.user.models.User;
-import com.stairway.data.source.user.models.UserResponse;
 import com.stairway.spotlight.R;
-import com.stairway.spotlight.core.FCMRegistrationIntentService;
-import com.stairway.spotlight.core.di.component.ComponentContainer;
 import com.stairway.spotlight.core.BaseActivity;
+import com.stairway.spotlight.core.di.component.ComponentContainer;
+import com.stairway.spotlight.screens.home.chats.ChatListFragment;
+import com.stairway.spotlight.screens.home.contacts.ContactListFragment;
+import com.stairway.spotlight.screens.home.profile.ProfileFragment;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import rx.Subscriber;
-
-import static com.stairway.spotlight.core.FCMRegistrationIntentService.SENT_TOKEN_TO_SERVER;
-
-public class HomeActivity extends BaseActivity{
-
-    UserSessionResult userSession;
-    UserAuthApi userAuthApi;
+public class HomeActivity extends BaseActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private View actionBarView;
+    private ActionBarDrawerToggle toggle;
+    private FloatingActionButton fab;
+    private Toolbar toolbar;
+    private static String TITLE_HOME = "Messages";
+    private static String TITLE_CONTACTS = "Contacts";
+    private static String TITLE_PROFILE = "Profile";
 
     public static Intent callingIntent(Context context) {
         Intent intent = new Intent(context, HomeActivity.class);
@@ -58,213 +38,129 @@ public class HomeActivity extends BaseActivity{
         return intent;
     }
 
-    View callView, chatView, contactView, profileView, actionBarView;
-    TextView callText, chatText, contactText, profileText;
-    ImageView callImage, chatImage, contactImage, profileImage;
-    ViewPager viewPager;
-    TabLayout tabLayout;
-    Drawable profileIcon, chatIcon, contactIcon, callIcon;
-    ImageButton actionBarIB;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        setContentView(R.layout.activity_home_drawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setChatFragment();
 
-        viewPager  = (ViewPager) findViewById(R.id.home_viewpager);
-        tabLayout  = (TabLayout) findViewById(R.id.home_sliding_tabs);
-
-        callView = LayoutInflater.from(this).inflate(R.layout.navigation_indicatior_text, null);
-        callText = (TextView) callView.findViewById(R.id.navigation_text);
-        callImage = (ImageView) callView.findViewById(R.id.navigation_icon);
-
-        chatView = LayoutInflater.from(this).inflate(R.layout.navigation_indicatior_text, null);
-        chatText = (TextView) chatView.findViewById(R.id.navigation_text);
-        chatImage = (ImageView) chatView.findViewById(R.id.navigation_icon);
-
-        contactView = LayoutInflater.from(this).inflate(R.layout.navigation_indicatior_text, null);
-        contactText = (TextView) contactView.findViewById(R.id.navigation_text);
-        contactImage = (ImageView) contactView.findViewById(R.id.navigation_icon);
-
-        profileView = LayoutInflater.from(this).inflate(R.layout.navigation_indicatior_text, null);
-        profileText = (TextView) profileView.findViewById(R.id.navigation_text);
-        profileImage = (ImageView) profileView.findViewById(R.id.navigation_icon);
-
-
-        profileIcon = ContextCompat.getDrawable(this, R.drawable.ic_profile_tab);
-        contactIcon = ContextCompat.getDrawable(this, R.drawable.ic_contacts_tab);
-        chatIcon = ContextCompat.getDrawable(this, R.drawable.ic_chat_tab);
-        callIcon = ContextCompat.getDrawable(this, R.drawable.ic_call_tab);
-
-        android.support.v7.app.ActionBar ab = getSupportActionBar();
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        int MyVersion = Build.VERSION.SDK_INT;
-
-        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        ab.setCustomView(R.layout.actionbar_home);
-        ab.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
-        ab.setElevation(0);
-        actionBarView = ab.getCustomView();
-        actionBarIB = (ImageButton) actionBarView.findViewById(R.id.actionbar_image);
-
-        // Permissions
-        if (MyVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
-            if (!checkIfAlreadyhavePermission()) {
-                requestForSpecificPermission();
-            }
-        }
-
-        viewPager.setOffscreenPageLimit(3);
-        viewPager.setAdapter(new HomePagerAdapter(getSupportFragmentManager(), HomeActivity.this));
-        tabLayout.setupWithViewPager(viewPager);
-
-        setTab(0, false);
-        setTab(1, false);
-        setTab(2, false);
-
-        viewPager.setCurrentItem(1);
-        // bug workaround
-        setTab(1, true);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                setTab(tab.getPosition(), true);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                setTab(tab.getPosition(), false);
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
+            public void onClick(View view) {
+                setContactsFragment();
             }
         });
 
-        Intent intent = new Intent(this, FCMRegistrationIntentService.class);
-        startService(intent);
-        FirebaseInstanceId instanceId = FirebaseInstanceId.getInstance();
-        String fCMToken = instanceId.getToken();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        //Upload to token to server if FCM token not updated
-        if(! sharedPreferences.getBoolean(SENT_TOKEN_TO_SERVER, false)) {
-            String fcmToken = sharedPreferences.getString(FCMRegistrationIntentService.FCM_TOKEN, "");
-            Logger.d("[HomeActivity] FCM TOKEN:"+fcmToken);
-            User updateUser = new User();
-            updateUser.setNotificationToken(fcmToken);
-            userAuthApi = new UserAuthApi();
-            userAuthApi.updateUser(updateUser, userSession.getAccessToken()).subscribe(new Subscriber<UserResponse>() {
-                @Override
-                public void onCompleted() {}
-                @Override
-                public void onError(Throwable e) {
-                    sharedPreferences.edit().putBoolean(SENT_TOKEN_TO_SERVER, true).apply();
-                }
-                @Override
-                public void onNext(UserResponse userResponse) {
-                    sharedPreferences.edit().putBoolean(SENT_TOKEN_TO_SERVER, true).apply();
-                }
-            });
-        }
-    }
-
-    /*
-    * Works only if all tabs are initialized first and then isActive set to 'true'.
-    * */
-    private void setTab(int tabPosition, boolean isActive){
-        switch (tabPosition){
-//            case 0:
-//                if(isActive) {
-//                    callIcon.setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN));
-//                    callText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
-//                    actionBarIB.setImageResource(R.drawable.ic_new_call);
-//                }
-//                else {
-//                    callIcon.setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(this, R.color.colorPrimaryDark), PorterDuff.Mode.SRC_IN));
-//                    callText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-//                }
-//                callImage.setImageDrawable(callIcon);
-//                callText.setText("Calls");
-//                tabLayout.getTabAt(0).setCustomView(callView);
-//                break;
-            case 1:
-                if(isActive) {
-                    chatIcon.setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN));
-                    chatText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
-                    actionBarIB.setImageResource(R.drawable.ic_new_chat);
-                }
-                else {
-                    chatIcon.setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(this, R.color.colorPrimaryDark), PorterDuff.Mode.SRC_IN));
-                    chatText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-                }
-                chatImage.setImageDrawable(chatIcon);
-                chatText.setText("Chats");
-                tabLayout.getTabAt(1).setCustomView(chatView);
-                break;
-            case 0:
-                if(isActive) {
-                    contactIcon.setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN));
-                    contactText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
-                    actionBarIB.setImageResource(R.drawable.ic_new_contact);
-                }
-                else {
-                    contactIcon.setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(this, R.color.colorPrimaryDark), PorterDuff.Mode.SRC_IN));
-                    contactText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-                }
-                contactImage.setImageDrawable(contactIcon);
-                contactText.setText("Contacts");
-                tabLayout.getTabAt(0).setCustomView(contactView);
-                break;
-            case 2:
-                if(isActive) {
-                    profileIcon.setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN));
-                    profileText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
-                    actionBarIB.setImageResource(R.drawable.ic_camera);
-                }
-                else {
-                    profileIcon.setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(this, R.color.colorPrimaryDark), PorterDuff.Mode.SRC_IN));
-                    profileText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-                }
-                profileImage.setImageDrawable(profileIcon);
-                profileText.setText("Profile");
-                tabLayout.getTabAt(2).setCustomView(profileView);
-                break;
-            default:
-                throw new IllegalArgumentException("Unspecified tab");
-        }
-    }
-
-    private boolean checkIfAlreadyhavePermission() {
-        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
-        if (result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else
-            return false;
-    }
-
-    private void requestForSpecificPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 101);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 101:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //granted
-                } else {
-                    //not granted
-                }
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            toggle.setDrawerIndicatorEnabled(true);
+            fab.setVisibility(View.VISIBLE);
+            toolbar.setTitle(TITLE_HOME);
+            super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_activity_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_contacts) {
+            setContactsFragment();
+
+        } else if (id == R.id.nav_profile) {
+            setProfileFragment();
+        } else if (id == R.id.nav_manage) {
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
     protected void injectComponent(ComponentContainer componentContainer) {
-        userSession = componentContainer.userSessionComponent().getUserSession();
+    }
+
+    private void setContactsFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.register_FragmentContainer, ContactListFragment.getInstance());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+        toggle.setDrawerIndicatorEnabled(false);
+//        android.support.v7.app.ActionBar ab = getSupportActionBar();
+//        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+//        ab.setCustomView(R.layout.actionbar_home);
+//        ab.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+//        ab.setElevation(0);
+//        actionBarView = ab.getCustomView();
+//        EditText search = (EditText) actionBarView.findViewById(R.id.actionbar_search);
+//        search.setHint("Search Contacts");
+
+        toolbar.setTitle(TITLE_CONTACTS);
+        fab.setVisibility(View.GONE);
+    }
+
+    private void setProfileFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.register_FragmentContainer, ProfileFragment.getInstance());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        toggle.setDrawerIndicatorEnabled(false);
+        toolbar.setTitle(TITLE_PROFILE);
+        fab.setVisibility(View.GONE);
+    }
+
+    private void setChatFragment() {
+        setSupportActionBar(toolbar);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.register_FragmentContainer, ChatListFragment.getInstance());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        toolbar.setTitle(TITLE_HOME);
     }
 }
