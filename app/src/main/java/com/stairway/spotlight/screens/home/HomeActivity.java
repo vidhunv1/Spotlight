6be0ca0a25f88f44eb0aa08cee0e54f65908f1ca
Vidhun <vidhunv1@gmail.com>
2016-11-03 20:02:57 +0530

    fix crash: FCM token upload

--- app/src/main/java/com/stairway/spotlight/screens/home/HomeActivity.java
+++ app/src/main/java/com/stairway/spotlight/screens/home/HomeActivity.java
@@ -1,10 +1,18 @@
 package com.stairway.spotlight.screens.home;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,17 +22,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.stairway.data.manager.Logger;
+import com.stairway.data.source.user.UserAuthApi;
+import com.stairway.data.source.user.UserSessionResult;
+import com.stairway.data.source.user.models.User;
+import com.stairway.data.source.user.models.UserResponse;
 import com.stairway.spotlight.R;
 import com.stairway.spotlight.core.BaseActivity;
+import com.stairway.spotlight.core.FCMRegistrationIntentService;
 import com.stairway.spotlight.core.di.component.ComponentContainer;
 import com.stairway.spotlight.screens.home.chats.ChatListFragment;
 import com.stairway.spotlight.screens.home.contacts.ContactListFragment;
 import com.stairway.spotlight.screens.home.profile.ProfileFragment;
 
+import rx.Subscriber;
+
+import static com.stairway.spotlight.core.FCMRegistrationIntentService.SENT_TOKEN_TO_SERVER;
+
 public class HomeActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private ActionBarDrawerToggle toggle;
     private FloatingActionButton fab;
+    UserAuthApi userAuthApi;
+    UserSessionResult userSession;
     private Toolbar toolbar;
     private static String TITLE_HOME = "Messages";
     private static String TITLE_CONTACTS = "Contacts";
@@ -43,6 +63,16 @@ public class HomeActivity extends BaseActivity
         setContentView(R.layout.activity_home_drawer);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Permissions
+        int MyVersion = Build.VERSION.SDK_INT;
+        if (MyVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (!checkIfAlreadyhavePermission()) {
+                requestForSpecificPermission();
+            }
+        }
+
         setChatFragment();
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -61,6 +91,54 @@ public class HomeActivity extends BaseActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Upload to token to server if FCM token not updated
+        if(! sharedPreferences.getBoolean(SENT_TOKEN_TO_SERVER, false)) {
+            String fcmToken = sharedPreferences.getString(FCMRegistrationIntentService.FCM_TOKEN, "");
+            Logger.d("[HomeActivity] FCM TOKEN:"+fcmToken);
+            User updateUser = new User();
+            updateUser.setNotificationToken(fcmToken);
+            userAuthApi = new UserAuthApi();
+            userAuthApi.updateUser(updateUser, userSession.getAccessToken()).subscribe(new Subscriber<UserResponse>() {
+                @Override
+                public void onCompleted() {}
+                @Override
+                public void onError(Throwable e) {
+                    sharedPreferences.edit().putBoolean(SENT_TOKEN_TO_SERVER, true).apply();
+                }
+                @Override
+                public void onNext(UserResponse userResponse) {
+                    sharedPreferences.edit().putBoolean(SENT_TOKEN_TO_SERVER, true).apply();
+                }
+            });
+        }
+    }
+
+    private boolean checkIfAlreadyhavePermission() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else
+            return false;
+    }
+
+    private void requestForSpecificPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 101:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //granted
+                } else {
+                    //not granted
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
@@ -92,8 +170,11 @@ public class HomeActivity extends BaseActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == android.R.id.home) {
+            Logger.d("[HomeActivity] backPressed");
+            onBackPressed();
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -117,10 +198,6 @@ public class HomeActivity extends BaseActivity
         return true;
     }
 
-    @Override
-    protected void injectComponent(ComponentContainer componentContainer) {
-    }
-
     private void setContactsFragment() {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.register_FragmentContainer, ContactListFragment.getInstance());
@@ -137,9 +214,11 @@ public class HomeActivity extends BaseActivity
         fragmentTransaction.replace(R.id.register_FragmentContainer, ProfileFragment.getInstance());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+
         toggle.setDrawerIndicatorEnabled(false);
         toolbar.setTitle(TITLE_PROFILE);
         fab.setVisibility(View.GONE);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void setChatFragment() {
@@ -149,5 +228,11 @@ public class HomeActivity extends BaseActivity
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
         toolbar.setTitle(TITLE_HOME);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void injectComponent(ComponentContainer componentContainer) {
+        userSession = componentContainer.userSessionComponent().getUserSession();
     }
 }
